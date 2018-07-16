14af92947e8235a7f889fd12862f2033da233f5a
Verena Blaschke <verena.blaschke@gmx.de>
2018-07-16 10:46:11 +0200

    family tree overview

---
+++ get_family_tree.py
@@ -0,0 +1,29 @@
+import csv
+
+DOCULECT_INFO = 'data/soundcomparisons_glottolog/glottolog_codes.csv'
+GLOTTOLOG_INFO = 'data/glottolog/languoid.csv'
+
+lang2parent = {}
+lang2name = {}
+with open(GLOTTOLOG_INFO, encoding='utf8') as f:
+    reader = csv.DictReader(f, delimiter=',')
+    for row in reader:
+        lang2name[row['id']] = row['name']
+        lang2parent[row['id']] = row['parent_id']
+
+
+with open(DOCULECT_INFO, encoding='utf8') as f:
+    reader = csv.DictReader(f, delimiter=',')
+    for row in reader:
+        lang = row['BDPA']
+        code = row['Code']
+        print(lang)
+        try:
+            print(code, lang2name[code])
+            while True:
+                code = lang2parent[code]
+                print(code, lang2name[code])
+        except KeyError:
+            pass
+        print()
+
