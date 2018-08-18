979e7d1b183a8e09042a313cc135d6d53265c8c7
Verena Blaschke <verena.blaschke@gmx.de>
2018-08-18 15:09:53 +0200

    Update results, add related work section

--- helper scripts/cosinesim2tikz.py
+++ helper scripts/cosinesim2tikz.py
@@ -41,20 +41,22 @@ doculect2coord = {}
 with open('helper scripts/coordinates.csv', encoding='utf8') as f:
     reader = csv.DictReader(f)
     for row in reader:
-        doculect2coord[row['doculect']] = (float(row['longitude']) - 4,
-                                           float(row['latitude']) - 46)
+        doculect2coord[row['doculect']] = (float(row['longitude']),
+                                           float(row['latitude']))
 
-with open('doc/figures/cosine.tex', 'w', encoding='utf8') as f:
+with open('doc/figures/cosine2.tex', 'w', encoding='utf8') as f:
     f.write("\\documentclass{standalone}\n")
     f.write("\\usepackage{tikz}\n")
     f.write("\\usepackage[outline]{contour}\n")
     f.write("\\begin{document}\n")
-    f.write("\\contourlength{0.1em}\n")
-    f.write("\\begin{tikzpicture}[scale=2.5, dot/.style={draw, circle, "
-            "minimum size=2mm, inner sep=0pt, outer sep=0pt, fill=red}]\n")
+    f.write("\\contourlength{0.2em}\n")
+    f.write("\\begin{tikzpicture}[scale=2.5, "
+            "dot/.style={draw=white, circle, minimum size=8pt, fill=red}, "
+            "doculect/.style={inner sep=1em}]\n\n")
+    f.write("% Establish doculect coordinates.\n")
     for doc, loc in doculect2coord.items():
-        f.write("\\node ({}) at {} {{}};\n"
-                .format(clean_node(doc), loc))
+        f.write("\\node ({}) at ({}, {}) {{}};\n"
+                .format(clean_node(doc), 0.7 * loc[0], loc[1]))
 
     docs = {i: d for i, d in enumerate(sorted(doculect2coord.keys(),
                                               key=lambda x: alias(x)))}
@@ -65,19 +67,23 @@ with open('doc/figures/cosine.tex', 'w', encoding='utf8') as f:
         for j in range(i + 1, n):
             # normalize similarity score
             d = (sim[i, j] - min_sim) / (max_sim - min_sim)
-            line = "\\draw[line width={}mm, color=black!70!blue!{}]" \
+            # if d < 0.75:
+            #     continue
+            line = "\\draw[line width={}mm, color=black!60!blue!{}]" \
                    "({}.center) -- ({}.center);\n" \
                    .format(d * 1.3, int(100 * d),
                            clean_node(docs[i]), clean_node(docs[j]))
             sims.append((d, line))
-    for _, line in sorted(sims):
+    f.write("\n% Draw similarity lines.\n")
+    for _, line in sorted(sims)[int(len(sims) * 0.9):]:
         f.write(line)
 
+    f.write("\n% Mark the locations with circles and labels.\n")
     for doc, loc in doculect2coord.items():
         f.write("\\node[dot] at ({}.center) {{}};\n"
                 .format(clean_node(doc)))
-        f.write("\\node[below] at ({}.center) "
-                "{{\\contour{{white}}{{\\Large {}}}}};\n"
+        f.write("\\node[doculect, right] at ({}.center) "
+                "{{\\contour{{white}}{{\\LARGE {}}}}};\n"
                 .format(clean_node(doc), clean_label(doc)))
     f.write("\\end{tikzpicture}\n")
     f.write("\\end{document}\n")
